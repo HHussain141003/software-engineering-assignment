1cdb482dc83e39119dc4c2a8f8a66713d4b0a772
Hanan Hussain <hhussain141003@gmail.com>
2024-11-21 18:54:19 +0000

    Initial work on Flask

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+from web_app import pages
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000, debug=True)
+
+print(pages.about)
