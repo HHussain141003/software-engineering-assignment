fcac9695e7fb21efa04455c59bbbc3863565ddb6
Hanan Hussain <hhussain141003@gmail.com>
2024-11-29 19:27:28 +0000

    Added placeholders for the screens in pages.py

--- web_app/pages.py
+++ web_app/pages.py
@@ -2,10 +2,32 @@ from flask import Blueprint
 
 bp = Blueprint("pages", __name__)
 
+# Login page
 @bp.route("/")
 def home():
     return "Hello, Home!"
 
-@bp.route("/about")
+# Home screen
+@bp.route("/home_screen")
 def about():
-    return "Hello, About!"
+    return
+
+# Create new ticket screen
+@bp.route("/add_ticket")
+def about():
+    return
+
+# View existing tickets screen
+@bp.route("/view_tickets")
+def about():
+    return
+
+# Admin screen
+@bp.route("/admin_screen")
+def about():
+    return
+
+# Log out screen
+@bp.route("/logout")
+def about():
+    return
