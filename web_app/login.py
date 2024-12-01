a1ba48fd26bced9d65f016f86e87f13ac8b828fd
Hanan Hussain <hhussain141003@gmail.com>
2024-12-01 16:07:42 +0000

    Continued working on login.py

--- web_app/login.py
+++ web_app/login.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from .database import get_db
 import logging
+from werkzeug.security import check_password_hash
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,9 +16,16 @@ def login():
     db = get_db()
 
     # Check username and password
-    user = db.execute("SELECT * FROM users WHERE username = ?", (username)).fetchone()
+    user = db.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone()
     if user is None:
         flash("Invalid username or password, please try again.")
         return render_template("login.html")
-        
+    
+    if not check_password_hash(user["password"], password):
+        flash("Invalid username or password, please try again.")
+        return render_template("login.html")
 
+    session["user_id"] = user["id"]
+    session["role"] = user["role"]
+    flash("Successfully logged in")
+    return redirect(url_for("pages.home_screen"))
