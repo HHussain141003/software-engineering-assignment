77dd8b7203066043186243b198928cfeb8f0a095
Hanan Hussain <hhussain141003@gmail.com>
2024-12-10 19:16:56 +0000

    Added the Username in the admin screen to view all tables

--- web_app/admin.py
+++ web_app/admin.py
@@ -6,6 +6,12 @@ admin_bp = Blueprint('admin_bp', __name__)
 @admin_bp.route('/admin_view_all_tickets')
 def admin_view_all_tickets():
     db = get_db()
-    tickets = db.execute('SELECT * FROM tickets').fetchall()
+    tickets = db.execute(
+        '''
+        SELECT tickets.*, users.username
+        FROM tickets
+        JOIN users ON tickets.user_id = users.id
+        '''
+    ).fetchall()
 
     return render_template('admin_screen.html', tickets=tickets)
