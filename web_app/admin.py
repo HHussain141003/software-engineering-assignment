416734c90b28a8bb2157c9f4f98ea58b060f707e
Hanan Hussain <hhussain141003@gmail.com>
2024-12-10 18:34:18 +0000

    Added the Admin View All Tickets screen successfully

--- web_app/admin.py
+++ web_app/admin.py
@@ -4,8 +4,9 @@ from .database import get_db
 admin_bp = Blueprint('admin_bp', __name__)
 
 @admin_bp.route('/admin_view_all_tickets')
-def admin_screen():
+def admin_view_all_tickets():
     db = get_db()
     tickets = db.execute('SELECT * FROM tickets').fetchall()
+    print("Tickets:", tickets)
 
     return render_template('admin_screen.html', tickets=tickets)
